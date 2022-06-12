8d946b262108505b8620cb6f165c837ec9e9df1c
MaXLab-OVGU <maxlab@ovgu.de>
2022-06-12 19:31:50 +0200

    Merged MPEG7 code - Modified

--- project_code.py
+++ project_code.py
@@ -299,6 +299,85 @@ def execute_Bovw(dataset, tam_patch, n_patches, random_state):
     print('Number of images and features = ', img_feats.shape)
     return img_feats
 
+# ------------------------- MPEG7 Color Layout Descriptor -------------
+
+def divide_blocks_and_get_dominant_color(image):
+    # Get Image dimensions
+    height, width, channels = image.shape
+
+    blocks = []
+    # Creating an 8x8 matrix
+    h = 8
+    v = 8
+
+    for i in range(v):
+        for j in range(h):
+            temp = image[int((height / v) * i): int((height / v) * (i + 1)),
+                   int((width / h) * j): int((width / h) * (j + 1))]
+            blocks.append(np.array(average_color(temp)))
+
+    return get_larger_image(blocks, h, v, 1)
+
+
+def average_color(temp):
+    # Get the average color from a block
+    avg_row = np.average(temp, axis=0)
+    avg_color = np.average(avg_row, axis=0)
+    return_img = np.ones((1, 1, 3), dtype=np.uint8)
+    return_img[:, :] = avg_color
+    return return_img
+
+
+def get_larger_image(blkList, horizontal, vertical, times):
+    overAllList = []
+    for i in range(horizontal):
+        eachRow = []
+        for j in range(vertical):
+            for k in range(times):
+                eachRow.append(blkList[i * horizontal + j][0][0])
+            temp = np.array(eachRow)
+        for k in range(times):
+            overAllList.append(temp)
+
+    return np.array(overAllList)
+
+
+def get_mpeg7_features(image):
+    # Image partitioning and representative color selection
+    representative_img = divide_blocks_and_get_dominant_color(image)
+
+    # Conversion of color space from RGB to YCbCr
+    YCbCr = cv2.cvtColor(representative_img, cv2.COLOR_BGR2YCrCb)
+
+    # Get the Y, Cb and Cr components
+    Y, Cb, Cr = YCbCr[:, :, 0] / 255.0, YCbCr[:, :, 1] / 255.0, YCbCr[:, :, 2] / 255.0
+
+    # DCT transformation of each component
+    dctY = cv2.dct(Y)
+    dctCb = cv2.dct(Cb)
+    dctCr = cv2.dct(Cr)
+
+    # Zigzag scanning of the transformed matrices\
+    scannedDctY = np.concatenate(
+        [np.diagonal(dctY[::-1, :], i)[::(2 * (i % 2) - 1)] for i in range(1 - dctY.shape[0], dctY.shape[0])])
+    scannedDctCb = np.concatenate(
+        [np.diagonal(dctCb[::-1, :], i)[::(2 * (i % 2) - 1)] for i in range(1 - dctCb.shape[0], dctCb.shape[0])])
+    scannedDctCr = np.concatenate(
+        [np.diagonal(dctCr[::-1, :], i)[::(2 * (i % 2) - 1)] for i in range(1 - dctCr.shape[0], dctCr.shape[0])])
+
+    return (scannedDctY, scannedDctCb, scannedDctCr)
+
+
+def execute_mpeg7(dataset):
+    features = []
+
+    for img in dataset:
+        features.append(get_mpeg7_features(img.img))
+
+    feature_arr = np.array(features)
+
+    return feature_arr
+
 # ------------------------- SURF Functions ----------------------------
 
 
@@ -328,16 +407,23 @@ def execute_surf(dataset, threshold):
 # ------------------------- Candidate Selection ----------------------------
 
 
-def euclidean_dist(img_feats, query_feats):
+def euclidean_dist(f_name, img_feats, query_feats):
     '''Calculates the Euclidean distance for the given features\n
        Parameters
            img_feats: candidate image features
            query_feats: query image feature
     '''
     dists = []
-    for i in range(n_imgs):
-        diq = np.sqrt(np.sum((img_feats[i]-query_feats)**2))
-        dists.append(diq)
+    if f_name == "BOVW":
+        for i in range(n_imgs):
+            diq = np.sqrt(np.sum((img_feats[i]-query_feats)**2))
+            dists.append(diq)
+    elif f_name == "MPEG7":
+        for i in range(n_imgs):
+            diq = np.sqrt(np.sum((img_feats[i][0] - query_feats[0]) ** 2)) + np.sqrt(
+                np.sum((img_feats[i][1] - query_feats[1]) ** 2)) + np.sqrt(
+                np.sum((img_feats[i][2] - query_feats[2]) ** 2))
+            dists.append(diq)
     return dists
 
 
@@ -362,10 +448,12 @@ def extract_feature_query(f_name, query_img):
         y = bovw_kmeans_model.predict(query_lbp)
         # computes descriptor
         query_feats, _ = np.histogram(y, bins=range(n_dic+1), density=True)
+    elif f_name == "MPEG7":
+        query_feats = get_mpeg7_features(query_img)
     return query_feats
 
 
-def select_candidates(k, feature_set, query_feature):
+def select_candidates(f_name, k, feature_set, query_feature):
     '''Generates Candidates for clustering using kNN\n
        Parameters
            k: k value for kNN
@@ -373,7 +461,7 @@ def select_candidates(k, feature_set, query_feature):
            query_feature: query image feature
     '''
     imgs = []
-    dists = euclidean_dist(feature_set, query_feature)
+    dists = euclidean_dist(f_name, feature_set, query_feature)
     k_cbir = np.argsort(dists)[:k]
     for i in range(k):
         imgs.append(image_dataset[k_cbir[i]])
@@ -485,6 +573,24 @@ def main():
         candidate_images_bovw, cand_img_bovw_descripList)
     print("Constraint Creation time: %0.3fs" % (time() - t0))
     print(cand_img_bovw_descripSet)
+
+    # ----------- MPEG7 Feature extraction ----------
+    t0 = time()
+    mpeg7_features = execute_mpeg7(image_dataset)
+    print("MPEG7 features Creation time: %0.3fs" % (time() - t0))
+    # ----------- Candidate Selection ---------------
+    t0 = time()
+    query_feature = extract_feature_query("MPEG7", query_img)
+    candidate_images_mpeg7 = select_candidates(
+        "MPEG7", k, mpeg7_features, query_feature)
+    print("Candidate Selection time: %0.3fs" % (time() - t0))
+    # ----------- Constraint Creation ---------------
+    t0 = time()
+    cand_img_mpeg7_descripList = generate_img_descrip(candidate_images_mpeg7)
+    cand_img_mpeg7_descripSet = process_img_descrip(
+        candidate_images_mpeg7, cand_img_mpeg7_descripList)
+    print("Constraint Creation time: %0.3fs" % (time() - t0))
+    print(cand_img_mpeg7_descripSet)
 
 
 if __name__ == "__main__":
